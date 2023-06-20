28af593c84f57742903a513701fc3d93ac30ac36
Miler Antonio de Azevedo <miler.azevedo@redesc.org.br>
2023-06-20 18:01:23 -0300

    adicionando worker

--- scripts/script.js
+++ scripts/script.js
@@ -5,21 +5,51 @@ const valorConvertidoH2 = document.getElementById('valorConvertido')
 currencyInput.addEventListener('input', formataMoeda);
 function formataMoeda(){
     const valor = parseFloat(currencyInput.value);
-    const valorFormatado = valor.toLocaleString('en-US', {
-        style:'currency',
-        currency: 'USD',
-    });
+    // const valorFormatado = valor.toLocaleString('en-US', {
+    //     style:'currency',
+    //     currency: 'USD',
+    // });
 
-    currencyInput.value = valorFormatado
+    // currencyInput.value = valorFormatado
 
-    converteValor(valorFormatado)
+    converteValor(valor)
 }
 
 function converteValor(valor){
-    const valorConvertido = toString(valor) * toString(cotacaoDolar)
-    console.log(valorConvertido)
+    const valorConvertido = parseFloat(valor) * parseFloat(cotacaoDolar)
 
     valorConvertidoH2.innerText = ''
-    valorConvertidoH2.innerText = valorConvertido
+    valorConvertidoH2.innerText = valorConvertido.toFixed(2)
+
+    if(currencyInput.value == ''){
+        valorConvertidoH2.innerText = cotacaoDolar
+    }
 
 }
+
+const moedaIfem = 'USD-BRL';
+
+
+async function conectaAPI (moeda){
+    const moedaSemIfem = moeda.replace('-', '');
+    const conecta = await fetch(`https://economia.awesomeapi.com.br/last/${moeda}`); // Exemplo moeda = USD-BRL
+    const conectaTraduzido = await conecta.json();
+    postMessage(conectaTraduzido); // exemplo moeda = USDBRL sem ifem
+    console.log(conectaTraduzido)
+};
+
+addEventListener("message", () =>{
+    conectaAPI(moedaIfem);
+    setInterval(()=> conectaAPI(moedaIfem), 5000);
+});
+
+
+let worker = new Worker('./scripts/workers/workerMoeda.js');
+worker.postMessage('usd');
+worker.addEventListener("message", event => {
+//   let tempo = geraHorario();
+  let valor = event.USDBRL.ask;
+  console.log(valor)
+//   selecionaCotacao("dolar", valor);
+//   adicionarDados(graficoParaDolar, tempo, valor);
+})
---
+++ scripts/workers/workerMoeda.js
@@ -0,0 +1,15 @@
+const moedaIfem = 'USD-BRL';
+
+
+async function conectaAPI (moeda){
+    const moedaSemIfem = moeda.replace('-', '');
+    const conecta = await fetch(`https://economia.awesomeapi.com.br/last/${moeda}`); // Exemplo moeda = USD-BRL
+    const conectaTraduzido = await conecta.json();
+    postMessage(conectaTraduzido); // exemplo moeda = USDBRL sem ifem
+    console.log(conectaTraduzido)
+};
+
+addEventListener("message", () =>{
+    conectaAPI(moedaIfem);
+    // setInterval(()=> conectaAPI(moedaIfem), 5000);
+});
