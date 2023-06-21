273b0051fec89d147b1eb27a6f1b30bfa544c829
Miler Antonio de Azevedo <miler.azevedo@redesc.org.br>
2023-06-21 18:05:17 -0300

    Atualizando valor do dólar em tempo real

--- scripts/script.js
+++ scripts/script.js
@@ -19,21 +19,23 @@ function formataMoeda(){
 function converteValor(valor){
     const valorConvertido = parseFloat(valor) * parseFloat(cotacaoDolar)
 
-    valorConvertidoH2.innerText = ''
-    valorConvertidoH2.innerText = valorConvertido.toFixed(2) + moeda
+    valorConvertidoH2.innerHTML = ''
+    valorConvertidoH2.innerHTML = valorConvertido.toFixed(2) + moeda
 
     if(currencyInput.value == ''){
-        valorConvertidoH2.innerText = parseFloat(cotacaoDolar).toFixed(2) + moeda
+        valorConvertidoH2.innerHTML = parseFloat(cotacaoDolar).toFixed(2) + moeda
     }
 
 }
 
 let worker = new Worker('./scripts/workers/workerMoeda.js');
 worker.postMessage('usd');
+setInterval(()=>worker.postMessage('usd'), 5000 )
 worker.addEventListener("message", event => {
   let valor = event.data.USDBRL.ask;
   cotacaoDolar = valor
-  console.log(cotacaoDolar)
-  valorConvertidoH2.innerText = parseFloat(cotacaoDolar).toFixed(2) + moeda
+
+  valorConvertidoH2.innerHTML = ''
+  valorConvertidoH2.innerHTML = parseFloat(valor).toFixed(2) + moeda
 
 })
